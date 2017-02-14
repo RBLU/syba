5b250a485e78f6b4f14a5ec9fafd8a01c6c6326a
RBLU <reto.blunschi@youpers.com>
2017-02-14 14:26:58 +0100

    formatting

--- src/common/services/batchrun.service.js
+++ src/common/services/batchrun.service.js
@@ -7,15 +7,15 @@ class BatchRunService {
 
     this.getRuns = (batchConfigId) => {
       return Restangular.all('batchruns').getList({ITSBATCHCONFIG: batchConfigId});
-    }
+    };
 
     this.getRun = (batchRunId) => {
       return Restangular.all('batchruns').one(batchRunId).get();
-    }
+    };
 
     this.ignoreRunInStats = (batchRunId, unignore) => {
       return Restangular.all('batchruns').one(batchRunId).customPUT({IGNOREINSTATS: unignore ? 0 : 1});
-    }
+    };
   }
 }
 
