8cb1cc681959a7dbc6e5f643c7d542f57d79e306
RBLU <reto.blunschi@youpers.com>
2017-02-19 16:44:30 +0100

    refactoring and commenting  before "zoom and pan"

--- src/common/kzhistorygraph/kzhistorygraph.controller.js
+++ src/common/kzhistorygraph/kzhistorygraph.controller.js
@@ -8,15 +8,21 @@ class KennzahlController {
 
     this.$onInit = () => {
 
+      // initialize fixed elements, drawing is then done in de redraw function
       let svg = d3.select($element.find('svg')[0])
           .attr('height', 560)
           .attr('width', 500),
         margin = {top: 20, right: 20, bottom: 60, left: 50},
         width = +svg.attr('width') - margin.left - margin.right,
-        height = +svg.attr('height') - margin.top - margin.bottom,
-        g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        height = +svg.attr('height') - margin.top - margin.bottom;
+
+      let viewport =
+        svg.append('g')
+          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+          .attr('class', 'viewport');
+      
       // add the tooltip area to the webpage
-      var tooltip = d3.select('body').append('div')
+      let tooltip = d3.select('body').append('div')
         .attr('class', 'tooltip')
         .style('opacity', 0);
 
@@ -30,7 +36,7 @@ class KennzahlController {
       function redraw(kz) {
 
         // clear the elements inside of the directive
-        g.selectAll('*').remove();
+        viewport.selectAll('*').remove();
 
         // set ranges (=screen dimensions) on scale functions
         let xScale = d3.scaleTime()
@@ -48,58 +54,59 @@ class KennzahlController {
         }));
 
         // compute min/max of graph to be shown
-        // to remove extreme outliers we show all data that is within
-        // 3 standard deviation of the of the mean.
+        /* currently unused        
         let mean = d3.mean(kz.history, (d) => {
           return +d.NUMBERVALUE;
         });
         let sd = d3.deviation(kz.history, (d) => {
           return +d.NUMBERVALUE;
         });
+        */
 
-        const dataExtent = d3.extent(kz.history, function (d) {
+        let yExtent = d3.extent(kz.history, function (d) {
           return +d.NUMBERVALUE;
         });
 
-        yScale.domain(dataExtent);
-        yScaleRects.domain(dataExtent);
+        yScale.domain(yExtent);
+        yScaleRects.domain(yExtent);
 
-        let cValue = function (d) {
-          return 'yellow';
-        };
-        let color = d3.scaleOrdinal(d3.schemeCategory10);
+
 
         // create the rects for the background color
         // 1st red one
-        g.append('rect')
+
+        let levels = viewport.append('g')
+          .attr('class', 'levels')
+
+        levels.append('rect')
           .attr('x', 0)
           .attr('width', width)
           .attr('y', height - yScaleRects(kz.LEVELLOWERROR))
           .attr('height', Math.max(0, yScaleRects(kz.LEVELLOWERROR)))
           .attr('class', 'error');
 
-        g.append('rect')
+        levels.append('rect')
           .attr('x', 0)
           .attr('width', width)
           .attr('y', height - yScaleRects(kz.LEVELLOWWARNING))
           .attr('height', Math.max(0, yScaleRects(kz.LEVELLOWWARNING) - yScaleRects(kz.LEVELLOWERROR)))
           .attr('class', 'warning');
 
-        g.append('rect')
+        levels.append('rect')
           .attr('x', 0)
           .attr('width', width)
           .attr('y', height - yScaleRects(kz.LEVELNORMAL))
           .attr('height', yScaleRects(kz.LEVELNORMAL) - Math.max(0, yScaleRects(kz.LEVELLOWWARNING)))
           .attr('class', 'normal');
 
-        g.append('rect')
+        levels.append('rect')
           .attr('x', 0)
           .attr('width', width)
           .attr('y', height - yScaleRects(kz.LEVELHIGHWARNING))
           .attr('height', yScaleRects(kz.LEVELHIGHWARNING) - yScaleRects(kz.LEVELNORMAL))
           .attr('class', 'warning');
 
-        g.append('rect')
+        levels.append('rect')
           .attr('x', 0)
           .attr('width', width)
           .attr('y', 0)
@@ -107,6 +114,7 @@ class KennzahlController {
           .attr('class', 'error');
 
         // create the line function, maps data to x/y coordinates
+        /* currently unused
         var line = d3.line()
           .x(function (d) {
             return xScale(new Date(d.STARTED));
@@ -114,7 +122,7 @@ class KennzahlController {
           .y(function (d) {
             return yScale(+d.NUMBERVALUE);
           });
-
+        */
 
         let xAxis =
           d3.axisBottom(xScale)
@@ -123,11 +131,15 @@ class KennzahlController {
             })
             .tickSizeInner(-height);
 
+        let yAxis = d3.axisLeft(yScale);
 
-        g.append('g')
+
+        viewport.append('g')
           .attr('transform', 'translate(0,' + height + ')')
-          .attr('class', 'x axis')
+          .attr('class', 'axis xaxis')
           .call(xAxis)
+
+          // format the tck texts by rotating them 45 degrees
           .selectAll('text')
           .attr('y', 15)
           .attr('x', 5)
@@ -135,8 +147,11 @@ class KennzahlController {
           .attr('transform', 'rotate(45)')
           .style('text-anchor', 'start');
 
-        g.append('g')
-          .call(d3.axisLeft(yScale))
+        viewport.append('g')
+          .attr('class', 'axis yaxis')
+          .call(yAxis)
+
+          // append a small text to the yAxis showing the Kennzahl
           .append('text')
           .attr('fill', '#000')
           .attr('transform', 'rotate(-90)')
@@ -154,7 +169,10 @@ class KennzahlController {
         //   .attr("stroke-width", 1.5)
         //   .attr("d", line);
 
-        g.selectAll('.dot')
+        viewport
+          .append('g')
+          .attr('class','dots')
+          .selectAll('.dot')
           .data(kz.history)
           .enter().append('circle')
           .attr('class', 'dot')
@@ -164,9 +182,6 @@ class KennzahlController {
           })
           .attr('cy', (d) => {
             return yScale(new Date(+d.NUMBERVALUE));
-          })
-          .style('fill', function (d) {
-            return color(cValue(d));
           })
           .on('mouseover', function (d) {
             tooltip.transition()
@@ -192,8 +207,10 @@ class KennzahlController {
         });
       }
 
-
     };
+
+
+
   }
 }
 
