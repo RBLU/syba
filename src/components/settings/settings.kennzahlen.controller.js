bf09ec0bba64db1e2a66b9c23f1cefd9c2b7e643
RBLU <reto.blunschi@youpers.com>
2017-02-21 13:15:51 +0100

    user feedback for kennzahl recalc

--- src/components/settings/settings.kennzahlen.controller.js
+++ src/components/settings/settings.kennzahlen.controller.js
@@ -21,8 +21,12 @@ class SettingsController {
 
     this.selectBatchConfig = (batchConfig) => {
       this.selectedbatchconfig = batchConfig;
-      this.currentkzs = _.filter(this.allkz, (kz) => {return kz.ITSSYRIUSBATCH == batchConfig.ITSSYRIUSBATCH || !kz.ITSSYRIUSBATCH;});
-      this.currentkzconfigs = _.filter(this.allkzconfigs, (kzc) => {return kzc.ITSBATCHCONFIG == batchConfig.BOID;});
+      this.currentkzs = _.filter(this.allkz, (kz) => {
+        return kz.ITSSYRIUSBATCH == batchConfig.ITSSYRIUSBATCH || !kz.ITSSYRIUSBATCH;
+      });
+      this.currentkzconfigs = _.filter(this.allkzconfigs, (kzc) => {
+        return kzc.ITSBATCHCONFIG == batchConfig.BOID;
+      });
       this.selectedkzconfig = undefined;
       this.selectedkz = undefined;
       this.kzcheckboxes = {};
@@ -32,9 +36,11 @@ class SettingsController {
     };
 
     this.selectKz = (kz) => {
-      this.selectedkzconfig = _.find(this.currentkzconfigs, (kzc) => {return kzc.ITSKENNZAHL == kz.BOID;});
+      this.selectedkzconfig = _.find(this.currentkzconfigs, (kzc) => {
+        return kzc.ITSKENNZAHL == kz.BOID;
+      });
       this.selectedkz = kz;
-      if (! this.selectedkzconfig) {
+      if (!this.selectedkzconfig) {
         // this is a newly selected KZ, we need to provide a new kzc for it
         this.selectedkzconfig = {
           ITSKENNZAHL: kz.BOID,
@@ -61,19 +67,39 @@ class SettingsController {
       } else {
         kennzahlenService.deleteKzConfig(this.selectedkzconfig);
         this.kzcheckboxes[kz.BOID] = undefined;
-        _.remove(this.allkzconfigs, (kzc) => {return kzc.BOID == this.selectedkzconfig.BOID;});
-        _.remove(this.currentkzconfigs, (kzc) => {return kzc.BOID == this.selectedkzconfig.BOID;});
+        _.remove(this.allkzconfigs, (kzc) => {
+          return kzc.BOID == this.selectedkzconfig.BOID;
+        });
+        _.remove(this.currentkzconfigs, (kzc) => {
+          return kzc.BOID == this.selectedkzconfig.BOID;
+        });
         this.selectedkzconfig = undefined;
-        this.selectedkz= undefined;
+        this.selectedkz = undefined;
       }
     };
 
+    let recalcRunning = false;
+
     this.recalckz = () => {
       $log.log('recalc kz: ', this.selectedkzconfig);
+      recalcRunning = true;
       kennzahlenService.recalcKennzahlConfig(this.selectedkzconfig)
         .then((result) => {
-          $log.log('recalc finsishd', result );
+          recalcRunning = false;
+          reloadKzConfig(this.selectedkzconfig.BOID);
+          $log.log('recalc finsishd', result);
+        })
+        .catch((err) => {
+          recalcRunning = false;
         });
+    };
+
+    this.getKzMessage = () => {
+      if (recalcRunning) {
+        return 'calculating...';
+      } else {
+        return 'Kennzahl noch nicht berechnet';
+      }
     };
 
     this.formCancel = () => {
@@ -83,16 +109,20 @@ class SettingsController {
     };
 
     $scope.$watch('vm.selectedkzconfig.BOID', (newBOID, oldBOID) => {
-      if (newBOID && newBOID.length >0 && newBOID !== oldBOID) {
-        kennzahlenService.getKennzahlConfigById(newBOID)
-          .then((kzc) => {
-            this.selectedkzconfig = kzc;
-          });
+      if (newBOID && newBOID.length > 0 && newBOID !== oldBOID) {
+        reloadKzConfig(newBOID);
       }
     });
 
+    const reloadKzConfig = (boid) => {
+      return kennzahlenService.getKennzahlConfigById(boid)
+        .then((kzc) => {
+          this.selectedkzconfig = kzc;
+        });
+    };
+
     this.formSubmit = () => {
-        // Todo: implement saving of kzform data
+      // Todo: implement saving of kzform data
       this.kzform.$setPristine();
       this.kzform.$setUntouched();
     };
